e499f2fa6379bb66af0e6796cedb897b4e62695f
Eric Ventor <ericventor97@gmail.com>
2024-04-11 19:30:22 -0700

    feat: ignore package-lock in dir search

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -29,8 +29,12 @@ local options = {
       height = 0.80,
       preview_cutoff = 120,
     },
+    find_files = {
+      hidden = true,
+      no_ignore_parent = true
+    },
     file_sorter = require("telescope.sorters").get_fuzzy_file,
-    file_ignore_patterns = { "node_modules" },
+    file_ignore_patterns = { "node_modules", "package%-lock.json" },
     generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
     path_display = { "truncate" },
     winblend = 0,
