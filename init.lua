86c5fbcd6eaf80e0f3422100c382ee99b3150923
Eric Ventor <ericventor97@gmail.com>
2024-09-03 11:44:02 -0700

    feat: close all buffers and remap find buffers

--- init.lua
+++ init.lua
@@ -101,6 +101,11 @@ vim.keymap.set('n', '<leader>bx', function()
   vim.cmd 'bd'
 end, { desc = '[B]uffer Close', noremap = true })
 
+-- Add this new keymap to close all buffers
+vim.keymap.set('n', '<leader>bX', function()
+  vim.cmd '%bd|e#|bd#'
+end, { desc = 'Close [B]uffer All', noremap = true })
+
 -- Map Ctrl+s to save
 vim.keymap.set('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
 vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>', { noremap = true, silent = true })
@@ -327,12 +332,13 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = '[ ] search files' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>bb', builtin.buffers, { desc = 'Find existing [B]uffers' })
       vim.keymap.set('n', '<leader>sg', function()
         builtin.live_grep {
           additional_args = { '--glob', '!package-lock.json' },
