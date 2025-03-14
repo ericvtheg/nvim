230343af2a4f07ebd3d2ad25584c0fcf62b8319e
Eric Ventor <eventor@metropolis.io>
2025-03-14 09:39:37 -0700

    add nvim lint

---
+++ lua/custom/plugins/nvim-lint.lua
@@ -0,0 +1,23 @@
+return {
+  'mfussenegger/nvim-lint',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    -- Set the environment variable for eslint_d
+    vim.env.ESLINT_D_PPID = vim.fn.getpid()
+
+    -- Configure linters for different filetypes
+    require('lint').linters_by_ft = {
+      javascript = { 'eslint_d' },
+      typescript = { 'eslint_d' },
+      javascriptreact = { 'eslint_d' }, -- for JSX
+      typescriptreact = { 'eslint_d' }, -- for TSX
+    }
+
+    -- Set up an autocmd to trigger linting
+    vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufReadPost', 'InsertLeave' }, {
+      callback = function()
+        require('lint').try_lint()
+      end,
+    })
+  end,
+}
