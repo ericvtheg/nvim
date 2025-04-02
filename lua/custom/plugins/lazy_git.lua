55caad9ee448337fcfb4ea6677ecc34bbd05ee5b
Eric Ventor <eventor@metropolis.io>
2025-04-02 09:54:48 -0700

    refactor: init and lazy_git

---
+++ lua/custom/plugins/lazy_git.lua
@@ -0,0 +1,20 @@
+return {
+  'jesseduffield/lazygit',
+  'kdheepak/lazygit.nvim',
+  cmd = {
+    'LazyGit',
+    'LazyGitConfig',
+    'LazyGitCurrentFile',
+    'LazyGitFilter',
+    'LazyGitFilterCurrentFile',
+  },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  -- setting the keybinding for LazyGit with 'keys' is recommended in
+  -- order to load the plugin when the command is run for the first time
+
+  keys = {
+    { '<leader>gi', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+  },
+}
