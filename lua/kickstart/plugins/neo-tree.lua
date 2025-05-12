10725b503c4f3ec036769ca84ab9f5d98aa0487d
Eric Ventor <ericventor97@gmail.com>
2025-05-11 19:43:46 -0700

    feat: scroll preview using ctrl d neo tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -6,7 +6,7 @@ return {
   version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'nvim-tree/nvim-web-devicons',
     'MunifTanjim/nui.nvim',
   },
   cmd = 'Neotree',
@@ -19,6 +19,7 @@ return {
       window = {
         mappings = {
           ['\\'] = 'close_window',
+          ['<C-d>'] = { 'scroll_preview', config = { direction = -10 } },
         },
       },
       group_empty_dirs = true,
