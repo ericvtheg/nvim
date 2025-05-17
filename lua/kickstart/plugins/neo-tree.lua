9dfe41605687579145e6daa1f5cd7e2ec0e64686
Eric Ventor <ericventor97@gmail.com>
2025-05-17 14:58:24 -0700

    feat: copy file path neotree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -20,6 +20,18 @@ return {
         mappings = {
           ['\\'] = 'close_window',
           ['<C-d>'] = { 'scroll_preview', config = { direction = -10 } },
+          ['y'] = function(state)
+            local node = state.tree:get_node()
+            local filename = node.name
+            vim.fn.setreg('"', filename)
+            vim.notify('Copied: ' .. filename)
+          end,
+          ['Y'] = function(state)
+            local node = state.tree:get_node()
+            local filepath = node:get_id()
+            vim.fn.setreg('"', filepath)
+            vim.notify('Copied: ' .. filepath)
+          end,
         },
       },
       group_empty_dirs = true,
