6b10d2cd0405bdf9a2352d4c1261ab6dcd94fc52
Eric Ventor <ericventor97@gmail.com>
2025-05-17 23:23:13 -0700

    chore: remove broken key mapping

--- init.lua
+++ init.lua
@@ -283,7 +283,6 @@ require('lazy').setup({
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         ['<leader>t'] = { name = '[T]erminal', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
         ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
         ['<leader>b'] = { name = '[B]uffer', _ = 'which_key_ignore' },
       }
