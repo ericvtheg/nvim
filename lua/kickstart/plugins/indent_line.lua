36fa23ce847baa9d93be7f3395b264038528b36d
Eric Ventor <ericventor97@gmail.com>
2025-05-17 18:54:19 -0700

    feat: decrease transparency of rainbow lines

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -2,7 +2,9 @@ return {
   'lukas-reineke/indent-blankline.nvim',
   main = 'ibl',
   opts = function(_, opts)
-    return require('indent-rainbowline').make_opts(opts)
+    return require('indent-rainbowline').make_opts(opts, {
+      color_transparency = 0.09,
+    })
   end,
   dependencies = {
     'TheGLander/indent-rainbowline.nvim',
