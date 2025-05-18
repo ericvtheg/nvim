fe488f75e5d41e81c7149dfd79a883e6b1690761
Eric Ventor <ericventor97@gmail.com>
2025-05-17 18:40:54 -0700

    feat: indent line

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -1,9 +1,37 @@
 return {
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
-  },
+  'lukas-reineke/indent-blankline.nvim',
+  main = 'ibl',
+  config = function()
+    -- Set the highlight groups
+    vim.cmd [[highlight IndentBlanklineIndent1 guifg=#30465d guibg=#14262b gui=nocombine]]
+    vim.cmd [[highlight IndentBlanklineIndent2 guifg=#30465d guibg=#09262e gui=nocombine]]
+    vim.cmd [[highlight IndentBlanklineIndent3 guifg=#30465d guibg=#161e37 gui=nocombine]]
+
+    local hooks = require 'ibl.hooks'
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      -- Ensure highlights persist across colorscheme changes
+      vim.cmd [[highlight IndentBlanklineIndent1 guifg=#30465d guibg=#14262b gui=nocombine]]
+      vim.cmd [[highlight IndentBlanklineIndent2 guifg=#30465d guibg=#09262e gui=nocombine]]
+      vim.cmd [[highlight IndentBlanklineIndent3 guifg=#30465d guibg=#161e37 gui=nocombine]]
+    end)
+
+    require('ibl').setup {
+      indent = {
+        char = '▎',
+        highlight = {
+          'IndentBlanklineIndent1',
+          'IndentBlanklineIndent2',
+          'IndentBlanklineIndent3',
+        },
+      },
+      whitespace = {
+        highlight = {
+          'IndentBlanklineIndent1',
+          'IndentBlanklineIndent2',
+          'IndentBlanklineIndent3',
+        },
+      },
+      scope = { enabled = false }, -- Disable if you're not using scope highlighting
+    }
+  end,
 }
