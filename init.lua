696cc9b31da59ca0a9d34d7cc7b9ce947899d769
Eric Ventor <ericventor97@gmail.com>
2025-05-17 17:45:10 -0700

    feat: theme tooling

--- init.lua
+++ init.lua
@@ -374,7 +374,11 @@ require('lazy').setup({
             },
           },
         },
-        --  pickers = {}
+        pickers = {
+          colorscheme = {
+            enable_preview = true,
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
@@ -395,6 +399,9 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>st', function()
+        vim.cmd 'Telescope colorscheme'
+      end, { desc = '[S]earch [T]hemes' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>bb', builtin.buffers, { desc = 'Find existing [B]uffers' })
       vim.keymap.set('n', '<leader>sa', function()
