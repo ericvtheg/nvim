dbc3beeb42e6985f102f24c6463ef4fd01ed7cd8
Eric Ventor <ericventor97@gmail.com>
2025-05-18 18:52:25 -0700

    chore: remove unused search

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -88,7 +88,6 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>st', function()
       vim.cmd 'Telescope colorscheme'
     end, { desc = '[S]earch [T]hemes' })
-    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader>bb', builtin.buffers, { desc = 'Find existing [B]uffers' })
     vim.keymap.set('n', '<leader>sa', function()
       builtin.live_grep {
