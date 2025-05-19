0a5622360df4c5733979e7a8aeefeebe2475aa61
Eric Ventor <ericventor97@gmail.com>
2025-05-18 18:40:06 -0700

    feat: smart open

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -76,7 +76,11 @@ return { -- Fuzzy Finder (files, lsp, etc)
     local builtin = require 'telescope.builtin'
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = '[ ] search files' })
+    vim.keymap.set('n', '<leader><leader>', function()
+      require('telescope').extensions.smart_open.smart_open {
+        cwd_only = true,
+      }
+    end, { noremap = true, silent = true, desc = '[ ] search files' })
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
