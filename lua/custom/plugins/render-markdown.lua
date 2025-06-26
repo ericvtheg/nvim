859e43e8750a321014c6cc26af40b6f9f0edfaa5
Eric Ventor <ericventor97@gmail.com>
2025-06-26 07:41:55 -0700

    fix: markdown rendering

--- lua/custom/plugins/render-markdown.lua
+++ lua/custom/plugins/render-markdown.lua
@@ -3,4 +3,18 @@ return {
   lazy = false,
   opts = {},
   ft = { 'markdown', 'codecompanion' },
+  config = function(_, opts)
+    require('render-markdown').setup(opts)
+
+    -- Ensure treesitter highlighting is enabled for markdown
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'markdown', 'codecompanion' },
+      callback = function()
+        vim.treesitter.start()
+      end,
+    })
+  end,
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter', -- Ensure treesitter is loaded
+  },
 }
