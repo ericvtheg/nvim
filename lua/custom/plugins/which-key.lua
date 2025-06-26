d9dc94b34c4879bd01c546f03a9ed81ef70892d5
Eric Ventor <ericventor97@gmail.com>
2025-06-26 07:42:01 -0700

    which key updated spec

--- lua/custom/plugins/which-key.lua
+++ lua/custom/plugins/which-key.lua
@@ -17,22 +17,32 @@ return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
   event = 'VimEnter', -- Sets the loading event to 'VimEnter'
   config = function() -- This is the function that runs, AFTER loading
-    require('which-key').setup()
+    require('which-key').setup(
 
     -- Document existing key chains
     require('which-key').register {
-      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ebug', _ = 'which_key_ignore' },
-      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      ['<leader>t'] = { name = '[T]erminal', _ = 'which_key_ignore' },
-      ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-      ['<leader>b'] = { name = '[B]uffer', _ = 'which_key_ignore' },
+      {
+        { '<leader>b', group = '[B]uffer' },
+        { '<leader>b_', hidden = true },
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>c_', hidden = true },
+        { '<leader>d', group = '[D]ebug' },
+        { '<leader>d_', hidden = true },
+        { '<leader>g', group = '[G]it' },
+        { '<leader>g_', hidden = true },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>r_', hidden = true },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>s_', hidden = true },
+        { '<leader>t', group = '[T]erminal' },
+        { '<leader>t_', hidden = true },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>w_', hidden = true },
+      },
     }
     -- visual mode
     require('which-key').register({
-      ['<leader>h'] = { 'Git [H]unk' },
+      { '<leader>h', desc = 'Git [H]unk', mode = 'v' },
     }, { mode = 'v' })
   end,
 }
