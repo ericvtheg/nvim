ebfa22f61e3936a01e9d12dcba3e76d224f7cd70
Eric Ventor <eventor@metropolis.io>
2025-06-27 16:02:44 -0700

    feat: example slash command git_files

--- lua/custom/plugins/code_companion.lua
+++ lua/custom/plugins/code_companion.lua
@@ -36,6 +36,25 @@ return {
       strategies = {
         chat = {
           adapter = adapter,
+          slash_commands = {
+            ['git_files'] = {
+              description = 'List git files',
+              ---@param chat CodeCompanion.Chat
+              callback = function(chat)
+                local handle = io.popen 'git ls-files'
+                if handle ~= nil then
+                  local result = handle:read '*a'
+                  handle:close()
+                  chat:add_reference({ content = result }, 'git', '<git_files>')
+                else
+                  return vim.notify('No git files available', vim.log.levels.INFO, { title = 'CodeCompanion' })
+                end
+              end,
+              opts = {
+                contains_code = false,
+              },
+            },
+          },
         },
         inline = {
           adapter = adapter,
