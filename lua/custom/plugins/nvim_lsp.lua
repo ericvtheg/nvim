becee0c1f705ebb9c52220f3590226bbce198e45
Eric Ventor <eventor@metropolis.io>
2025-09-05 16:14:34 -0700

    chore: lsp config init tweaks and typescript tools

--- lua/custom/plugins/nvim_lsp.lua
+++ lua/custom/plugins/nvim_lsp.lua
@@ -2,7 +2,9 @@ return { -- LSP Configuration & Plugins
   'neovim/nvim-lspconfig',
   dependencies = {
     -- Automatically install LSPs and related tools to stdpath for Neovim
-    { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
+    -- Mason must be loaded before its dependents so we need to set it up here.
+    -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
+    { 'mason-org/mason.nvim', opts = {} },
     { 'mason-org/mason-lspconfig.nvim' },
     'WhoIsSethDaniel/mason-tool-installer.nvim',
 
@@ -190,13 +192,6 @@ return { -- LSP Configuration & Plugins
       -- pyright = {},
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`tsserver`) will work just fine
-      ['typescript-language-server'] = {},
-
       lua_ls = {
         -- cmd = {...},
         -- filetypes = { ...},
@@ -219,14 +214,6 @@ return { -- LSP Configuration & Plugins
       server_config.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server_config.capabilities or {})
       require('lspconfig')[server_name].setup(server_config)
     end
-
-    -- Ensure the servers and tools above are installed
-    --  To check the current status of installed tools and/or manually install
-    --  other tools, you can run
-    --    :Mason
-    --
-    --  You can press `g?` for help in this menu.
-    require('mason').setup()
 
     -- You can add other tools here that you want Mason to install
     -- for you, so that they are available from within Neovim.
